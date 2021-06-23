c207cbaea560cc0322c31a96cc15bc060b46d948
Eamonnnnn <84051595+Eamonnnnn@users.noreply.github.com>
2021-06-23 08:46:57 +0800

    Delete P30 3-4.py

--- P30 3-4.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-from thinkdsp import decorate, read_wave
-
-wave=read_wave(r'C:\Users\Mia\Desktop\study\code_72475__rockwehrmann__glissup02.wav')
-wave.make_spectrogram(512).plot(high=5000)
-decorate(xlabel='Time (s)', ylabel='Frequency (Hz)')
-plt.show()
